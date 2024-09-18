c9f8728ecfa4c021661ef067e2f79c4fa62afa0c
GaGaGaGala <kosachevagalina@example.com>
2024-09-19 02:40:36 +0300

    Добавление

--- test.py
+++ test.py
@@ -1,9 +1,33 @@
 import unittest
-import main
-import data_download
+from data_download import fetch_stock_data, add_moving_average, notify_if_strong_fluctuations
+from data_plotting import create_and_save_plot
+import pandas as pd
+import os
 
 
-class DataTest(unittest.TestCase):
-    def test_fetch_stock_data(self):
-        st = stock_data
-        self.assertTrue(stock_data)
+class Close(unittest.TestCase):
+    def setUp(self):
+        self.ticker = 'AAPL'
+        self.period = '1mo'
+        self.threshold = 5
+
+        # Получение исторических данных об акциях для указанного тикера и временного периода
+
+    def test_return_dataframe(self):
+        stock_data = fetch_stock_data(self.ticker, self.period)
+        self.assertIsInstance(stock_data, pd.DataFrame)
+
+        # Добавление в DataFrame колонки со скользящим средним, рассчитанным на основе цен закрытия
+
+    def test_add_moving_average(self):
+        stock_data = fetch_stock_data(self.ticker, self.period)
+        stock_data = add_moving_average(stock_data)
+        self.assertTrue('Moving_Average' in stock_data.columns)
+
+        # Проверка на вывод данных в консоль
+
+    def test_output_to_console(self):
+        stock_data = fetch_stock_data(self.ticker, self.period)
+
+if __name__ == '__main__':
+    unittest.main()
